4cde68050ac9ae868e6f95540799a10b19d5a339
ErkanDemircioglu <64330625+ErkanDemircioglu@users.noreply.github.com>
2021-01-21 19:45:39 +0300

    adding files

---
+++ MyDictionary/MyDictionaryDemo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    public class MyDictionaryDemo<K,V>
+    {
+        K[] _key;
+        V[] _value;
+
+        public MyDictionaryDemo()
+        {
+            _key = new K[0];
+            _value = new V[0];
+        }
+
+        public void Add(K key,V value)
+        {
+            K[] tempKey = _key;
+            V[] tempValue = _value;
+
+            _key = new K[_key.Length + 1];
+            _value = new V[_value.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                _key[i] = tempKey[i];
+                _value[i] = tempValue[i];
+            }
+
+            _key[_key.Length - 1] = key;
+            _value[_value.Length - 1] = value;
+        }
+
+        public int Count
+        {
+            get { return _key.Length; }
+        }
+
+        public K[] Key
+        {
+            get { return _key; }
+        }
+        public V[] Value
+        {
+            get { return _value; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> musteriler = new Dictionary<int, string>();
+            musteriler.Add(0, "Erkan");
+            musteriler.Add(1, "Ali");
+            musteriler.Add(2, "Ayşe");
+            musteriler.Add(3, "Fatma");
+
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("ID= "+ musteri.Key + "   Ad= " + musteri.Value); 
+            }
+
+            Console.WriteLine("---------------My Dictionary----------------");
+            MyDictionaryDemo<int, string> musteriler2 = new MyDictionaryDemo<int, string>();
+
+            musteriler2.Add(0, "Erkan");
+            musteriler2.Add(1, "Ali");
+            musteriler2.Add(2, "Ayşe");
+            musteriler2.Add(3, "Fatma");
+
+            for (int i = 0; i < musteriler2.Count; i++)
+            {
+                Console.WriteLine("ID= "+musteriler2.Key[i]+"  Ad= "+musteriler2.Value[i]);
+            }
+
+
+            Console.ReadLine();
+ 
+        }
+    }
+}
